704ff00277acee55373d0497e968e8bba02151b1
rupesh1310 <a.rupesh66@gmail.com>
2019-01-12 02:55:45 +0530

    first commit

---
+++ block.js
@@ -0,0 +1,24 @@
+class Block {
+	constructor(timestamp, lastHash, hash, data) {
+		this.timestamp = timestamp;
+		this.lastHash = lastHash;
+		this.hash = hash;
+		this.data = data;
+	}
+
+	toString() {
+		return `Block -
+			Timestamp: ${this.timestamp}
+			Last Hash: ${this.lastHash.substring(0,10)}
+			Hash 	 : ${this.hash.substring(0,10)}
+			data	 : ${this.data}`;
+		
+	}
+
+}
+
+module.exports = Block;
+
+
+
+
