7c49928355f1db6123b5f8577ff79a9e65f90a95
rupesh1310 <a.rupesh66@gmail.com>
2019-01-12 03:26:10 +0530

    SHA256 fuction

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 	constructor(timestamp, lastHash, hash, data) {
 		this.timestamp = timestamp;
@@ -22,11 +24,16 @@ static genesis() {
 	static mineBlock(lastBlock, data) {
 		const timestamp = Date.now();
 		const lastHash = lastBlock.hash;
-		const hash = 'todo-hash';
+		const hash = Block.hash(timestamp, lastHash,data);
+
 
 		return new this(timestamp, lastHash, hash, data);
 	}
+	static hash(timestamp, lastHash, data) {
+		return SHA256(`${timestamp}${lastHash}${data}`).toString();	
+	}
 }
+
 
 module.exports = Block;
 
