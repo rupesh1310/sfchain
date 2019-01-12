6626c495b5c72016ff96fbc75cf13a6ea0ee8ac2
rupesh1310 <a.rupesh66@gmail.com>
2019-01-13 01:26:25 +0530

    testing chain validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -24,4 +24,16 @@ describe('Blockchain', () => {
 		expect(bc.isValidChain(bc2.chain)).toBe(true);
 
 	});
+
+	it('invalidates a chain with a corrupt genesis block', () => {
+		bc2.chain[0].data = 'Bad data';
+		expect(bc.isValidChain(bc2.chain)).toBe(false);
+	});
+
+	it('invalidates a corrupt chain', () => {
+		bc2.addBlock('foo');
+		bc2.chain[1].data = 'Not foo';
+		expect(bc.isValidChain(bc2.chain)).toBe(false);
+	});
 });
+
