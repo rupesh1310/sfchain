fedf58c5d48e37f6851d36626e4b6a9e7eed71b8
rupesh1310 <a.rupesh66@gmail.com>
2019-01-16 01:18:22 +0530

    testing nonce functionality

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
 let data, lastBlock, block;
@@ -14,5 +15,8 @@ expect(block.data).toEqual(data);
 	
 it('sets the `lastHash` to match the hash of the last block', () => {
 expect(block.lastHash).toEqual(lastBlock.hash);
+});
+	it('generates a hash that matches the difficulty', () => {
+	expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
 });
 });
---
+++ config.js
@@ -0,0 +1,2 @@
+const DIFFICULTY = 4;
+module.exports = { DIFFICULTY };
