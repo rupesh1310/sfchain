1151cef442406342f43cf7c7869b26e00bb4f7b5
rupesh1310 <a.rupesh66@gmail.com>
2019-01-15 23:19:45 +0530

    sync bchain across peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -27,18 +27,25 @@ class P2pServer {
   connectSocket(socket) {
     this.sockets.push(socket);
     console.log('Socket connected');
-    
     this.messageHandler(socket);
-
-    socket.send(JSON.stringify(this.blockchain.chain));
+    this.sendChain(socket);
   }
 
   messageHandler(socket) {
   	socket.on('message', message => {
   	const data = JSON.parse(message);
   	console.log('data', data);
-
+ 	this.blockchain.replaceChain(data);
   	});
+  }
+
+sendChain(socket) {
+socket.send(JSON.stringify(this.blockchain.chain));
+}
+
+
+  syncChains(){
+  	this.sockets.forEach(socket => this.sendChain(socket));
   }
 
 }
