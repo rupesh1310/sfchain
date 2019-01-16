f2a325e14ec3b6db1c6c114426bb125ab8260f51
rupesh1310 <a.rupesh66@gmail.com>
2019-01-16 21:43:04 +0530

    chainutil and keygen

--- wallet/index.js
+++ wallet/index.js
@@ -1,17 +1,18 @@
+const ChainUtil = require('../chain-util');
 const { INITIAL_BALANCE } = require('../config');
 
 class Wallet {
-	constructor() {
-		this.balance = INITIAL_BALANCE;
-		this.keypair = null;
-		this.publickey = null;
-	}
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = ChainUtil.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
+  }
 
-toString() {
-	return `Wallet - 
-	publickey: ${this.publickey.toString()}
-	balance  : ${this.balance}`
- }
+  toString() {
+    return `Wallet -
+      publicKey: ${this.publicKey.toString()}
+      balance  : ${this.balance}`
+  }
 }
 
 module.exports = Wallet;
