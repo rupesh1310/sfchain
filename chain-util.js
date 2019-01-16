1487f9859a677534d39af61d05c77f585126144d
rupesh1310 <a.rupesh66@gmail.com>
2019-01-17 00:01:42 +0530

    testing transactions

--- chain-util.js
+++ chain-util.js
@@ -1,4 +1,5 @@
 const EC = require('elliptic').ec;
+const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1');
 
 class ChainUtil 
@@ -6,6 +7,11 @@ class ChainUtil
 	static genKeyPair() {
 		return ec.genKeyPair();
 	}
+	static id() {
+		return uuidV1();
+	}
+
+
 }
 
 module.exports = ChainUtil;
